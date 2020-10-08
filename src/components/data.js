cd5239e421e69da3ac5b4e088ce125f3a720582b
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-10-08 06:28:37 -0700

    joseph-vega

--- src/components/data.js
+++ src/components/data.js
@@ -3,9 +3,12 @@ import React from "react"
 export default function Data(props) {
     console.log(props);
     return (
-        <div>
-        {props.nasaData.title}
-        <img src={props.nasaData.url} alt="galaxy"/>
+        <div className="container">
+            <h1>{props.nasaData.title}</h1>
+            <h3>Date: {props.nasaData.date}</h3>
+            <img src={props.nasaData.url} alt="galaxy" />
+            <p>{props.nasaData.explanation}</p>
+            <h4>Copyright: {props.nasaData.copyright}</h4>
         </div>
     );
 }
