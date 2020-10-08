895dbed2562a048c8fc24fbc76d21bf52d56cdc0
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-10-08 14:59:47 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -22,11 +22,7 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      {/* <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p> */}
+    <div>
       <Data nasaData={nasaData}/>
     </div>
   );
--- src/components/data.js
+++ src/components/data.js
@@ -1,14 +1,52 @@
 import React from "react"
+import styled from 'styled-components'
+
+const Body = styled.div`
+    color: white;
+    height: 110vh;
+    line-height: 2;
+`
+
+const Div = styled.div`
+    margin: 0 auto;
+    width: 60%;
+`
+
+const HeadingH1 = styled.h1`
+    margin-top: 0;
+    text-align: center;
+`
+
+const H3 = styled.h3`
+    margin-top: 0;
+    text-align: center;
+    margin-bottom: 5%;
+`
+
+const Image = styled.img`
+    width: 100%; 
+    margin-bottom: 2%;
+`
+
+const P = styled.h4 `
+    letter-spacing: 2;
+`
+
+const H4 = styled.h4`
+    margin-top: 7%;
+`
 
 export default function Data(props) {
     console.log(props);
     return (
-        <div className="container">
-            <h1>{props.nasaData.title}</h1>
-            <h3>Date: {props.nasaData.date}</h3>
-            <img src={props.nasaData.url} alt="galaxy" />
-            <p>{props.nasaData.explanation}</p>
-            <h4>Copyright: {props.nasaData.copyright}</h4>
-        </div>
+        <Body>
+            <Div>
+            <HeadingH1>{props.nasaData.title}</HeadingH1>
+            <H3>Date: {props.nasaData.date}</H3>
+            <Image src={props.nasaData.url} alt="galaxy" />
+            <P>{props.nasaData.explanation}</P>
+            <H4>Copyright: {props.nasaData.copyright}</H4>
+            </Div>
+        </Body>
     );
 }
